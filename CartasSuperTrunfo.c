9291750eaee00db0abdca693b1363371b691ba6f
jluiz-programacao <202503027871@alunos.estacio.br>
2025-03-19 14:51:01 +0000

    Código básico montado.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,24 +8,22 @@
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-int main() {
-    
-        char A[10], B[10]; //estados estão representados por letras A, B ... com até 10 carateres//
-        char A01[10], B04[10]; //codigo da carta//
-        char cidade1[10], cidade2[10]; //nome de cidade pode ter até 10 caracteres//
-        int Populacao_A01, Populacao_B04; //será pedido o tamanho da população//
-        float Area_A01, Area_B04; //em Km²//
-        float PIB_A01, PIB_B04;
-        int Numero_de_pontos_turisticos;
+
+int  main(){
+        char estado1[40], estado2[40]; 
+        char codigo1[40], codigo2[10]; 
+        char cidade1[10], cidade2[10]; 
+        int populacao1, populacao2; 
+        float area1, area2; 
+        float PIB1, PIB2;
+        int pontos_turisticos1, pontos_turisticos2;
+        float densidade_demografica1;
+        float densidade_demografica2;
+        float PIBpercapta1;
+        float PIBpercapta2;
+
            
-        //declaração de variáveis de cálculo de atributos// 
-
-        float DensidadePopulacional_A01 = Populacao_A01 / Area_A01;
-        float PIBpercapta_A01 = PIB_A01 / Populacao_A01;
-        float DensidadePopulacional_B04 = Populacao_B04 / Area_B04;
-        float PIBpercapta_B04 = PIB_B04 / Populacao_B04;
-
-
+        
        
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
@@ -33,14 +31,14 @@ int main() {
     
     
     
-    printf("CARTA 1 \n");
+    printf("*** CARTA 1 ***\n\n\n");
 
     printf("Digite o nome do Estado: \n"); //saída da interpelação do nome do estado//
-    scanf("%s", &A, &B); //entrada do usuário com até 10 caracteres (variável char)//
+    scanf("%s", &estado1); //entrada do usuário com até 10 caracteres (variável char)//
 
 
     printf("Digite o código da sua carta: \n"); //saída da interpelação do código da carta//
-    scanf("%s", &A01, &B04); //entrada do código da carta (variável char)//
+    scanf("%s", &codigo1); //entrada do código da carta (variável char)//
 
 
     printf("Digite o nome da cidade: \n"); //saída da interpelação do nome da cidade//
@@ -48,31 +46,34 @@ int main() {
 
 
     printf("Digite o tamanho da população da cidade: \n"); //saída da interpelação da população//
-    scanf("%d", &Populacao_A01); //entrada do tamanho da população (variável inteira)
+    scanf("%d", &populacao1); //entrada do tamanho da população (variável inteira)
 
 
     printf("Digite o tamanho da área territorial: \n"); //saída da interpelação sobre a área//
-    scanf("%f", &Area_A01); //entrada do tamanho da área (varilavel float)//
+    scanf("%f", &area1); //entrada do tamanho da área (varilavel float)//
 
 
     printf("Digite o valor do PIB: \n"); //saída da interpelação do PIB//
-    scanf("%f", &PIB_A01); //entrada do valor do PIB (variável float)
+    scanf("%f", &PIB1); //entrada do valor do PIB (variável float)
 
 
     printf("Digite o número de pontos turísticos do município: \n"); //saída da interpelação dos pontos turísticos//
-    scanf("%d", &Numero_de_pontos_turisticos); //entrada do número de pontos turísticos
-    printf("\n"); //comando para pular linha e melhorar a organização//
-   
+    scanf("%d", &pontos_turisticos1); //entrada do número de pontos turísticos
+    printf("\n\n"); //comando para pular linha e melhorar a organização//
+ 
+    //declaração de variáveis para cálculo de atributos// 
+    densidade_demografica1 = (float) populacao1 / area1;
+    PIBpercapta1 = (float) PIB1 /  populacao1;
 
-    printf("CARTA 2 \n");
 
+    printf("*** CARTA 2 ***\n\n\n");
 
     printf("Digite o nome do Estado: \n"); //início da sequência referente à carta 2 - repetiçao de códigos//
-    scanf("%s", &B);
+    scanf("%s", &estado2);
 
 
     printf("Digite o código da sua carta: \n");
-    scanf("%s", &B04);
+    scanf("%s", &codigo2);
 
 
     printf("Digite o nome da cidade: \n");
@@ -80,55 +81,54 @@ int main() {
 
 
     printf("Digite o tamanho da população da cidade: \n");
-    scanf("%d", &Populacao_B04);
+    scanf("%d", &populacao2);
 
 
     printf("Digite o tamanho da área territorial: \n");
-    scanf("%f", &Area_B04);
+    scanf("%f", &area2);
 
 
     printf("Digite o valor do PIB: \n");
-    scanf("%f", &PIB_B04);
+    scanf("%f", &PIB2);
 
 
     printf("Digite o número de pontos turísticos do município: \n");
-    scanf("%d", &Numero_de_pontos_turisticos);
-    printf("\n");
+    scanf("%d", &pontos_turisticos2);
+    printf("\n\n");
 
-         DensidadePopulacional_A01 = Populacao_A01 / Area_A01;
-         PIBpercapta_A01 = PIB_A01 / Populacao_A01;
-         DensidadePopulacional_B04 = Populacao_B04 / Area_B04;
-         PIBpercapta_B04 = PIB_B04 / Populacao_B04;
+    densidade_demografica2 = (float) populacao2 / area2;
+    PIBpercapta2 = (float) PIB2 /  populacao2;
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    
     printf("carta 1:\n"); //impressão com saída "carta 1"//
-    printf("Estado: %s\n", A); //impressão com saída do nome do estado//
-    printf("Código: %s\n", A01); //impressão com saída do código da carta//
+    printf("Estado: %s\n", estado1); //impressão com saída do nome do estado//
+    printf("Código: %s\n", codigo1); //impressão com saída do código da carta//
     printf("Nome da Cidade: %s\n", cidade1); //impressão com saída do nome da cidade//
-    printf("População: %d\n", Populacao_A01); //impressão com saída da população//
-    printf("Área: %f km²\n", Area_A01); //impressão com saída da área; acrescentei a unidade depois da variável para sais na impressão//
-    printf("PIB: %f bilhões de reais\n", PIB_A01); //impressão com saída do valor do PIB; acrescentei a unidade depois da variável para sais na impressão//
-    printf("Número de Pontos Turísticos: %d\n", Numero_de_pontos_turisticos); //impressão com saída dos pontos turísticos//
-    printf("DensidadePopulacional: %e\n", DensidadePopulacional_A01);
-    printf("PIBpercapta: %e\n", PIBpercapta_A01);
-    printf("\n"); //comando para melhorar organização - pular linha//
+    printf("População: %d\n", populacao1); //impressão com saída da população//
+    printf("Área: %.2f km²\n", area1); //impressão com saída da área; acrescentei a unidade depois da variável para sais na impressão//
+    printf("PIB: %.2f bilhões de reais\n", PIB1); //impressão com saída do valor do PIB; acrescentei a unidade depois da variável para sais na impressão//
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1); //impressão com saída dos pontos turísticos//
+    printf("Densidade Populacional: %.2f hab/Km2\n", densidade_demografica1);
+    printf("PIB per capta: %.2f reais\n", PIBpercapta1);
+    printf("\n\n"); //comando para melhorar organização - pular linha//
 
 
     printf("carta 2:\n");
-    printf("Estado: %s\n", B);
-    printf("Código: %s\n", B04);
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
-    printf("População: %d\n", Populacao_B04);
-    printf("Área: %f km²\n", Area_B04);
-    printf("PIB: %f bilhões de reais\n", PIB_B04);
-    printf("Número de Pontos Turísticos: %d\n", Numero_de_pontos_turisticos);
-    printf("DensidadePopulacional: %f\n", DensidadePopulacional_B04);
-    printf("PIBpercapta: %f\n", PIBpercapta_B04);
-    printf("\n");
-    printf("\n");
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", PIB2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade Populacional: %.2f\n", densidade_demografica2);
+    printf("PIB per capta: %.2f\n", PIBpercapta2);
+    printf("\n\n");
+    
 
   
 
